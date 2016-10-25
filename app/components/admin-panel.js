5a945d3c8b88966ed7a2199b424a56a215e5349c
Aimen Khakwani and Marta Sánchez <aimen+marta@>
2016-10-25 15:03:02 -0700

    Add styling and everything else

---
+++ app/components/admin-panel.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateArticleForm: false,
+  actions: {
+    update(article, params) {
+      this.sendAction('update', article, params);
+    },
+    delete(article) {
+      if (confirm('Are you sure you want to delete this article?')) {
+        this.sendAction('destroyArticle', article);
+      }
+    }
+  }
+});
